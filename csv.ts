8757874098ca679a6f23bf294b62c8212c782112
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-01-04 17:26:50 +0100

    c

--- csv.ts
+++ csv.ts
@@ -3,9 +3,13 @@ export type RecordCSV = [nom: string, taille: number, naissance: Date];
 export type CSV = [titres: Titles, ...records: RecordCSV[]];
 
 export function stringToRecord(str: string): RecordCSV {
-    return ['', NaN, new Date()];
-}
-
-export function stringToCSV(str: string): CSV {
-    return [['', '', '']];
-}
+    const [n, t, d] = str.split(";").map(t => t.trim() );
+    return [n, +t, new Date(d)];
+  }
+  
+  export function stringToCSV(str: string): CSV {
+    const [titres, ...lines] = str.trim().split("\n");
+    const titles = titres.split(";").map(t => t.trim()) as Titles;
+    return [titles, ...lines.map( stringToRecord )];
+  }
+  
